0a5858e50e5bcfb95b074a68e30ed3b1aa80c12c
Zesen Qian <github@riaqn.org>
2021-11-28 22:55:03 +0100

    fancy meta-programming in color conversion

--- src/nvidia_codec/color.py
+++ src/nvidia_codec/color.py
@@ -35,30 +35,98 @@ class Converter:
         h = self.height = st.height
         sf = self.source_format = type(source_template)
         sp = self.source_pitch = st.pitch
+
+        os = target_format(w, h)
+        tf = self.target_format = target_format
+        tp = self.target_pitch = os.calculate_pitch()        
         log.debug(f'source pitch {sp}')
 
-        assert sf is SurfaceNV12,  "Only support NV12 as input"
-        assert target_format is SurfaceRGB24, "Only support RGB24 as output"
         assert target_space == c.AVCOL_SPC_RGB, "Only support RGB as output"
-        assert target_range == c.AVCOL_RANGE_JPEG, "Only support JPEG (full range) as output"
+
+        if sf in [SurfaceP016, SurfaceYUV444_16Bit]:
+            stype = 'uint16_t'
+        elif sf in [SurfaceNV12, SurfaceYUV444]:
+            stype = 'uint8_t'
+        else:
+            raise Exception(f"Unsupported source format {sf}")
+
+        if tf in [SurfaceRGB48, SurfaceRGB444P16]:
+            ttype = 'uint16_t'
+            normalize_rgb = f'''
+            {ttype} R = min(max(R_ * 65536, .5), 65536 - .5);
+            {ttype} G = min(max(G_ * 65536, .5), 65536 - .5);
+            {ttype} B = min(max(B_ * 65536, .5), 65536 - .5);
+            '''
+        elif tf in [SurfaceRGB24, SurfaceRGB444P]:
+            ttype = 'uint8_t'
+            normalize_rgb = f'''
+            {ttype} R = min(max(R_ * 256, .5), 256 - .5);
+            {ttype} G = min(max(G_ * 256, .5), 256 - .5);
+            {ttype} B = min(max(B_ * 256, .5), 256 - .5);
+            '''
+        else:
+            raise Exception(f"Unsupported target format {tf}")
+
+        if stype == 'uint16_t' and ttype == 'uint16_t':
+            mtype = 'double'
+        else:
+            mtype = 'float'
+
+        if sf in [SurfaceNV12, SurfaceP016]:
+            load_yuv = f'''
+            {stype} Y = (({stype}*)(src + x * {sp}))[y];
+            uint8_t *src_uv = src + {sp} * {h};
+            {stype} U = (({stype}*)(src_uv + x/2 * {sp}))[y/2*2];
+            {stype} V = (({stype}*)(src_uv + x/2 * {sp}))[y/2*2 + 1];
+            '''
+        else:
+            raise Exception(f"Unsupported source format {sf}")
+
+        if source_range == c.AVCOL_RANGE_MPEG:
+            # partial range
+            normalize_yuv = f'''
+                {mtype} Y_ = ({mtype})(Y - 16) / 220;
+                {mtype} U_ = ({mtype})(U - 128) / 225;
+                {mtype} V_ = ({mtype})(V - 128) / 225;
+            '''
+        else:
+            # full range
+            normalize_yuv = f'''
+                {mtype} Y_ = ({mtype})(Y) / 256;
+                {mtype} U_ = ({mtype})(U - 128) / 256;
+                {mtype} V_ = ({mtype})(V - 128) / 256;
+            '''
 
         if source_space in [c.AVCOL_SPC_BT470BG, c.AVCOL_SPC_SMPTE170M]:
             m = [[1, 0, 1.402], [1, -0.34414, -0.71414], [1, 1.772, 0]]
         elif source_space == c.AVCOL_SPC_BT709:
             m = [[1, 0, 1.5748], [1, -0.1873, -0.4681], [1, 1.8556, 0]]
+        elif source_space == c.AVCOL_SPC_BT2020_NCL:
+            # https://gist.github.com/yohhoy/dafa5a47dade85d8b40625261af3776a
+            m = [[1, 0, 1.4746], [1, -0.1646, -0.5714], [1, 1.8814, 0]]
         else:
             raise Exception(f"Unsupported color space {source_space}")
 
-        if source_range == c.AVCOL_RANGE_MPEG:
-            # limited range: Y is [16, 235], Cb and Cr are [16, 240]            
-            f = False 
-        else:
-            f = True # source is full range
+        yuv_to_rgb = f'''
+        {mtype} R_ = {m[0][0]} * Y_ + {m[0][1]} * U_ + {m[0][2]} * V_;
+        {mtype} G_ = {m[1][0]} * Y_ + {m[1][1]} * U_ + {m[1][2]} * V_;
+        {mtype} B_ = {m[2][0]} * Y_ + {m[2][1]} * U_ + {m[2][2]} * V_;
+        '''
 
-        os = target_format(w, h)
-        tf = self.target_format = target_format
-        tp = self.target_pitch = os.calculate_pitch()
-        log.debug(f'target pitch = {tp}')
+        if tf in [SurfaceRGB24, SurfaceRGB48]:
+            store_rgb = f'''
+            (({ttype}*)(dst + x * {tp}))[y * 3] = R;
+            (({ttype}*)(dst + x * {tp}))[y * 3 + 1] = G;
+            (({ttype}*)(dst + x * {tp}))[y * 3 + 2] = B;
+            '''
+        elif tf in [SurfaceRGB444P, SurfaceRGB444P16]:
+            store_rgb = f'''
+            (({ttype}*)(dst + x * {tp}))[y] = R;
+            (({ttype}*)(dst + h * {tp} + x * {tp}))[y] = G;
+            (({ttype}*)(dst + h * {tp} * 2 + x * {tp}))[y] = B;
+            '''
+        else:
+            raise Exception(f"Unsupported target format {tf}")
 
         self.mod =  SourceModule(f"""
         #include <stdint.h>
@@ -67,23 +135,11 @@ class Converter:
         if (x >= {h}) return;
         int y = blockIdx.y * blockDim.y + threadIdx.y; // column
         if (y >= {w}) return;
-        float Y = src[x * {sp} + y];
-        float Y_ = {'Y/256' if f else '(Y - 16)/220'};
-        uint8_t *src_uv = src + {sp} * {h};
-        float U = src_uv[x/2 * {sp} + y/2*2];
-        float U_ = {'(U-128)/256' if f else '(U - 128)/225'};
-        float V = src_uv[x/2 * {sp} + y/2*2 + 1];
-        float V_ = {'(V-128)/256' if f else '(V - 128)/225'};
-        // Y_, U_, V_ are in range [0, 1)
-
-        float R_ = {m[0][0]} * Y_ + {m[0][1]} * U_ + {m[0][2]} * V_;
-        float G_ = {m[1][0]} * Y_ + {m[1][1]} * U_ + {m[1][2]} * V_;
-        float B_ = {m[2][0]} * Y_ + {m[2][1]} * U_ + {m[2][2]} * V_;
-        // R_, G_, B_ should be in range [0, 1)
-
-        dst[x * {tp} + y * 3] = min(max(R_ * 256, .5), 255.5);
-        dst[x * {tp} + y * 3 + 1] = min(max(G_ * 256, .5), 255.5);
-        dst[x * {tp} + y * 3 + 2] = min(max(B_ * 256, .5), 255.5);
+        {load_yuv}
+        {normalize_yuv}
+        {yuv_to_rgb}
+        {normalize_rgb}
+        {store_rgb}
     }}
     """)
         self.convert = self.mod.get_function("convert")
